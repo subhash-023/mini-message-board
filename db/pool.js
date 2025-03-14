0d99fb64d9ee10b7406e4b601d8a63c4c335ec52
Subhash <subhash02305@gmail.com>
2025-03-14 11:50:59 +0000

    Store connection string as environment variables.
    Instead of hardcoding the credentials of deployed database.
    A environment variable is created to store the connection string.

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,6 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  connectionString: "postgresql://postgres:fUcRSsmEjCHznPKLewoNJqsBsamMzRrK@switchyard.proxy.rlwy.net:52426/railway"
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
